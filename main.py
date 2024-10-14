cac47a33ab0794d16092694d8821dd12793496af
anis <anishachmioui@gmail.com>
2024-10-14 12:23:29 +0200

    question_1

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import numpy as np
 import scipy as sp
 import pandas as pd
-
-# pd.set_option('display.max_rows', 500)
-# pd.set_option('display.max_columns', 500)
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('TkAgg')  # or 'Agg' for non-interactive backend
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
 
 def Datamatrix_Y(str):
     """Loads the datamatrix and y vector from csv file"""
     csv_data = pd.read_csv('data.csv')
+    print(csv_data)
     X_matrix = csv_data.iloc[:, :-1].values
     y_vector=csv_data.iloc[:,-1].values
+
     X_matrix=np.array(X_matrix)
     y_vector=np.array(y_vector)
+    print(y_vector)
     return X_matrix,y_vector
 
 
 X_mat,y_vec=Datamatrix_Y('data.csv')
+csv_data = pd.read_csv('data.csv')
+csv_data=np.array(csv_data)
+print("-1", csv_data[csv_data==-1])
+plt.plot(X_mat)
+plt.show()
 
 
 print(X_mat)
